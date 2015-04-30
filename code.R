f4fda22dc5188834d4d39f9ef4062e9587fb716e
Huaraz2 <adam.pohl9999@gmail.com>
2015-04-30 17:41:03 +0100

    added some more stuff

--- code.R
+++ code.R
@@ -92,13 +92,13 @@ pie3D(table(adult[, "Sex"]), main = "Spread of gender through the Census",  col 
 table(adult[, "Sex"])
 
                     #Chapter 3 Research questions
-#Is there a difference in Capital gains between Males and Females?
+"Q1: Is there a difference in Capital gains between Males and Females?"
 Sex = adult[, "Sex"]
 CapitalGain = adult[,"Capital-Gain"]
 SexVCapitalGain = table(CapitalGain,Sex)
-barplot(SexVCapitalGain[2:119,1], beside = TRUE, main = "Spread of Capital Gains for Females", col = rainbow(50))
-barplot(SexVCapitalGain[2:119,2], beside = TRUE, main = "Spread of Capital Gains for Males", col = rainbow(60))
-barplot(SexVCapitalGain[1,], beside = TRUE, main = "Zero Capital Gains for Males and Females", col = rainbow(2))
+barplot(SexVCapitalGain[2:119,1], xlab = "Capital Gains", ylab = "Number of people", beside = TRUE, main = "Spread of Capital Gains for Females", col = rainbow(50))
+barplot(SexVCapitalGain[2:119,2], xlab = "Capital Gains", ylab = "Number of people", beside = TRUE, main = "Spread of Capital Gains for Males", col = rainbow(60))
+barplot(SexVCapitalGain[1,], xlab = "Capital Gains", ylab = "Number of people", beside = TRUE, main = "Zero Capital Gains for Males and Females", col = rainbow(2))
 
 #My original thought was to go through the differences and work something from there but this never panned out but hey it is nice code so I left it in.
 Difference = {}
@@ -113,16 +113,17 @@ nrow(MaleCapitalGain)
 nrow(FemaleCapitalGain)
 
 
-var.test(SexVCapitalGain[,1], SexVCapitalGain[,2])
+
 #Females
 shapiro.test(SexVCapitalGain[,1])
 symmetry.test(SexVCapitalGain[,1])
+
 #Males
 shapiro.test(SexVCapitalGain[,2])
 symmetry.test(SexVCapitalGain[,2])
 
 "H0: There is no difference in Capital Gains for Males and Females."
-"H1: There is a difference in Capital Gains for Males and Females."
+"HA: There is a difference in Capital Gains for Males and Females."
 
 SvCG = c(SexVCapitalGain[,1], SexVCapitalGain[,2])
 MeanDiff = rep(0,1000)
@@ -131,7 +132,7 @@ for (i in 1:1000){
   MeanDiff[i] = mean(SvCG[s[1:119]]) - mean(SvCG[s[120:238]])
 }
 originalMeandiff = mean(SvCG[1:119]) - mean(SvCG[120:238])
-#If this is > 0.05 then we can accept the null hypothesis that there is no link between the gender of a person and their capital gain.
+#If pval < 0.05 then we can reject the null hypothesis that there is no link between the gender of a person and their capital gain.
 pval = length(MeanDiff[MeanDiff >= originalMeandiff]) / 1000
 if(pval > 0.05){
   "As the pval > 0.05 there is not enough evidence to discard H0."
@@ -141,9 +142,7 @@ if(pval > 0.05){
 pval
 
 
-#Does the Race affect the number of hours per week people work?
-# cor(adult[,12], as.numeric(adult[,8]))
-# cor.test(adult[,12],as.numeric(adult[,8]))
+"Q2: Does the Race affect the number of hours per week people work?"
 RvNHW = lm(adult[,12]~adult[,8])
 summary(RvNHW)
 
@@ -154,9 +153,54 @@ plot(RvNHW, which = 5:6)
 
 
                     #Chapter 4 Research questions
-#Can we build a model to see which variables effect the income?
-model = lm(as.numeric(adult[,14])~ adult[,1] + adult[,2] + adult[,3] + adult[,4] + adult[,6] + adult[,7] + adult[,8] + adult[,9] + adult[,10] + adult[,11] + adult[,12] + adult[,13])
+"Q3: Can we build a model to see which variables effect the income?"
+model = lm(as.numeric(adult[,14])~ adult[,1] + adult[,2] + adult[,4] + adult[,6] + adult[,8] + adult[,9] + adult[,10] + adult[,11] + adult[,12] + adult[,13])
 summary(model)
 
+#From the regression table we can imply that there is little or no link between a person's native country and their income. Therefore we will delete this from our model and work out a new regression table.
 
-#Test if Education and Capital gain are related. You can use as many of the other variables in your model you want.
+model2 = lm(as.numeric(adult[,14])~ adult[,1] + adult[,2] + adult[,4] + adult[,6] + adult[,8] + adult[,9] + adult[,10] + adult[,11] + adult[,12])
+summary(model2)
+
+#Unsurpringly as we just got rid of native country, we are able to imply from the new regression table that the Race of someone has little or no effect on their income.  We will therefore remove Race and try again.
+
+model3 = lm(as.numeric(adult[,14])~ adult[,1] + adult[,2] + adult[,4] + adult[,6] + adult[,9] + adult[,10] + adult[,11] + adult[,12])
+summary(model3)
+
+#From the regression table for model3 we can see that the next variable that we should get rid of is Workclass (adult[, 2]) as we can infer that it does not affect the income.
+
+model4 = lm(as.numeric(adult[,14])~ adult[,1] + adult[,4] + adult[,6] + adult[,9] + adult[,10] + adult[,11] + adult[,12])
+summary(model4)
+
+#From the regression table we can imply that all of the variables in model4 are signifcant in deciding what a person's income will be.  we will now perform a Principal Componant Analysis to check if what variables are actually linked to the income.
+
+model5 = subset(adult, select = c('Age', 'EduNum', 'Capital-Gain', 'Capital-Loss', 'HPW'))
+occupation = as.numeric(adult[, "Occupation"])
+sex = as.numeric(adult[, "Sex"])
+
+model5 = cbind(model5, occupation, sex)
+PCA = prcomp(model5[,1:7])
+PCA
+summary(PCA)
+par(mfrow = c(1,1))
+plot(PCA$sd, type = "l", xlab = "Component number", ylab = "Eigenvalues", main = "Scree plot")
+
+
+
+"Q4: Test if Education and Capital gain are related. You can use as many of the other variables in your model you want."
+model = lm(adult[, 10]~adult[, 4])
+summary(model)
+
+# x = {}
+# for (i in 1:32561) {
+#   if(adult[i, "EduCat"] != "Bachelors") {
+#     if(adult[i, "EduCat"] != "Doctorate") {
+#       if(adult[i, "EduCat"] != "Masters") {
+#         if(adult[i, "EduCat"] != "Prof-school"){
+#           x = rbind(x, adult[i,])
+#         }
+#       }
+#     }
+#   }
+# }
+# summary(x)
